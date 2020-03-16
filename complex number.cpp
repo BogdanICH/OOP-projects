7c1f84b47fb7c6c2625a02d25898444ba028d99b
Iordache Bogdan-Mihai <48602122+BogdanICH@users.noreply.github.com>
2020-03-16 11:19:54 +0200

    Update complex number.cpp

--- complex number.cpp
+++ complex number.cpp
@@ -29,7 +29,6 @@ public:
     {
         imaginar = im;
     }
-
 };
 
 class VectorDeComplecsi:public NumarComplex
@@ -52,7 +51,7 @@ int main()
 {
     NumarComplex A, B;
     int menu, n, m, ok = 1;
-    while(ok == 1)
+    while (ok == 1)
     {
         cout<<" 1. Suma a doua numere complexe \n 2. Determinarea modulului unui numar complex \n 3. Produsul a doua numere complexe \n 4. Determinarea vectorului modulelor \n 5. Vectorul sortat \n 6. Suma elementelor vect. de complecsi \n 7. Produsul scalar a doi vectori de complecsi";
         cout<<"\n Meniu = ";
@@ -92,7 +91,7 @@ int main()
             cout<<"Dati re & im pentru fiecare numar complex din vector: ";
             cin>>C;
             C.vectModule();
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
                 cout<<C.vectorModule[i]<<" | ";
 
             break;
@@ -139,7 +138,6 @@ int main()
         cout<<"\nContinui? (1/0)";
         cin>>ok;
     }
-
     return 0;
 }
 
@@ -207,17 +205,16 @@ VectorDeComplecsi::~VectorDeComplecsi()
 istream &operator>>(istream &in, VectorDeComplecsi &A)
 {
 
-    for(int i = 0; i < A.n; i++)
+    for (int i = 0; i < A.n; i++)
     {
         in>>A.v[i];
     }
     return in;
 }
 
-
 ostream &operator<<(ostream &o, VectorDeComplecsi &A)
 {
-    for(int i = 0; i < A.n; i++)
+    for (int i = 0; i < A.n; i++)
     {
         o<<A.v[i]<<"\n";
     }
@@ -227,7 +224,7 @@ ostream &operator<<(ostream &o, VectorDeComplecsi &A)
 void VectorDeComplecsi::vectModule()
 {
 
-    for(int i = 0; i < this->n; i++)
+    for (int i = 0; i < this->n; i++)
     {
         this->vectorModule[i] = this->v[i].detModul();
     }
@@ -237,10 +234,10 @@ void VectorDeComplecsi::vectModule()
 void VectorDeComplecsi::sortare()
 {
     NumarComplex aux;
-    for(int i = 1; i < this->n; i++)
-        for(int j = 0; j < this->n - i; j++)
+    for (int i = 1; i < this->n; i++)
+        for (int j = 0; j < this->n - i; j++)
         {
-            if(this->vectorModule[j] > this->vectorModule[j+1])
+            if (this->vectorModule[j] > this->vectorModule[j+1])
             {
                 swap(this->v[j], this->v[j+1]);
             }
@@ -252,7 +249,7 @@ void VectorDeComplecsi::sortare()
 NumarComplex VectorDeComplecsi::suma()
 {
     NumarComplex S;
-    for(int i = 0; i < this->n; i++)
+    for (int i = 0; i < this->n; i++)
     {
         S = S + this->v[i];
     }
@@ -262,6 +259,6 @@ NumarComplex VectorDeComplecsi::suma()
 
 void VectorDeComplecsi::produs(VectorDeComplecsi &A, VectorDeComplecsi &B)
 {
-    for(int i = 0; i < this->n; i++)
+    for (int i = 0; i < this->n; i++)
         this->v[i] = A.v[i] * B.v[i];
 }
